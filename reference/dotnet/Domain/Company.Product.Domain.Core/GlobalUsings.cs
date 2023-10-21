ec0a82ce691e091367aa47ecd5a5ac14118d2016
Arjan Geertsema <arjan@synion.nl>
2023-10-21 11:53:13 +0200

    added core domain

---
+++ reference/dotnet/Domain/Company.Product.Domain.Core/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using Company.Product.Domain.UseCases;
+global using Company.Product.Domain.UseCases.Types;
+global using Company.Product.Domain.Core.UseCases;
+global using Microsoft.Extensions.DependencyInjection;
---
+++ reference/dotnet/Domain/Company.Product.Domain.Core/UseCases/GetAnimalsUseCase.cs
@@ -0,0 +1,9 @@
+﻿namespace Company.Product.Domain.Core.UseCases;
+
+public class GetAnimalsUseCase : IGetAnimalsUseCase
+{
+    public Task<IEnumerable<Animal>> GetAnimals(string filter, int limit, int offset, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
