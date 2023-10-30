6d38b0c7c2ef13288c3706e7cc2b89d9e8021754
Sardar Yumatov <yumatov.s@tkppensioen.nl>
2023-10-30 19:03:19 +0100

    Container application

---
+++ reference/springboot/runtime-app/src/test/java/org/example/ContextLoadsTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ContextLoadsTest {
+
+    @Test
+    void contextLoads() {
+        // no need to test anything, if we reach this line, then Spring is up and running
+    }
+}
